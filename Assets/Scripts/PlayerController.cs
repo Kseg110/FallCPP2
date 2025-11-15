429f8ce9113ec88ecd497807aabff2bc3b2bce2b
Kyle S <kylseg@icloud.com>
2025-11-15 18:16:40 -0500

    Add input and weapon system scripts
    Introduces InputManager and InputSystem_Actions for handling player input, along with Sword and WeaponBase scripts for weapon mechanics. Also adds a Singleton utility and updates PlayerController to integrate the new input system.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,29 +8,95 @@ public class PlayerController : MonoBehaviour
     Vector3 moveInput;
     public float moveSpeed = 8f;
     public float headHeight = 1.8f;
+    private Pickup nearbyPickup;
 
     Enemy lastFrozenEnemy = null;
     public Transform cameraTransform; // Assign camera in the player inspector for synced movement 
 
-    // void Awake()
-    //{
-    //    input = new InputSystem_Actions();
-    //    input.Player.SetCallbacks(this);
-    //}
+    private CharacterController cc;
+    private Camera mainCamera;
+    private Animator anim;
+    private WeaponBase curWeapon = null;
 
+    [SerializeField] private Transform weaponAttachPoint;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    LayerMask weaponLayerMask;
+    LayerMask enemyLayerMask;
+
+    private float curSpeed = 2.0f;
+    [Header("Movement Settings")]
+    [SerializeField] private float initSpeed = 2.0f;
+    [SerializeField] private float maxSpeed = 15.0f;
+    [SerializeField] private float moveAccel = 2f;
+    [SerializeField] private float rotationSpeed = 5.0f;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpHeight = 2.0f;
+    [SerializeField] private float timeToJumpApex = 0.4f;
+
+    //Movement variables
+    float gravity;
+    float initialJumpVelocity;
+
+    Vector2 direction; //direction of movement - no gravity is applied here
+    Vector3 velocity;
+
+    bool jumpPressed = false;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        try
+        {
+            cc = GetComponent<CharacterController>();
+            anim = GetComponentInChildren<Animator>();
+            if (cc == null) throw new UnassignedReferenceException("CharacterController component is not assigned!");
+        }
+        catch (UnassignedReferenceException e)
+        {
+            //do something here
+            UnityEngine.Application.Quit();
+        }
+        finally
+        {
+            //this code always runs after the try-catch block no matter if an exeption was thrown or not
+        }
+
+        mainCamera = Camera.main;
+        CalculateJumpVariables();
+
+        //Layer 6 is Weapons
+        //LayerMask.GetMask("Weapon") seems to be inconsistent (in testing it was pulling layer 64) - setting it directly works better
+        weaponLayerMask = 6;
+        enemyLayerMask = 3;
+
+        InputManager.Instance.OnMoveEvent += OnMove;
+        InputManager.Instance.OnJumpEvent += OnJump;
+        InputManager.Instance.OnDropEvent += OnDrop;
     }
 
+    void OnValidate()
+    {
+        CalculateJumpVariables();
+    }
+
+    public void OnJump(bool pressed) => jumpPressed = pressed;
+    public void OnMove(Vector2 movementDir) => direction = movementDir;
+
+    public void OnDrop(bool pressed)
+    {
+        if (pressed && curWeapon != null)
+        {
+            curWeapon.Drop(GetComponent<Collider>());
+            curWeapon = null;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         // input axes
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
+        //float hInput = Input.GetAxis("Horizontal");
+        //float vInput = Input.GetAxis("Vertical");
 
         // camera direction synced to movement 
         Vector3 camForward = cameraTransform.forward;
@@ -39,7 +106,7 @@ public class PlayerController : MonoBehaviour
         camForward.Normalize();
         camRight.Normalize();
 
-        moveInput = (camForward * vInput + camRight * hInput).normalized;
+        //moveInput = (camForward * vInput + camRight * hInput).normalized;
         //moveInput = new Vector3(hInput, 0f, vInput);
 
         //Raycast
@@ -68,12 +135,32 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        if (nearbyPickup != null && Input.GetKeyDown(KeyCode.E))
+        {
+            nearbyPickup.OnPickup();
+            Destroy(nearbyPickup.gameObject);
+            nearbyPickup = null;
+        }
+
     }
 
     private void FixedUpdate()
     {
-        Vector3 moveVelocity = moveInput * moveSpeed;
-        rb.linearVelocity = new Vector3(moveVelocity.x, rb.angularVelocity.y, moveVelocity.z);
+        //Vector3 moveVelocity = moveInput * moveSpeed;
+        //rb.linearVelocity = new Vector3(moveVelocity.x, rb.angularVelocity.y, moveVelocity.z);
+        //apply movement
+        Vector3 projectedMoveDir = ProjectedMoveDirection();
+        UpdateCharacterVelocity(projectedMoveDir);
+
+        cc.Move(velocity * Time.fixedDeltaTime);
+        anim.SetFloat("speed", curSpeed / maxSpeed);
+
+        //apply rotation
+        if (direction != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(projectedMoveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
     }
 
 
@@ -84,6 +171,80 @@ public class PlayerController : MonoBehaviour
             // End the game: reload the scene (or implement your own game over logic)
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        // Detect Pickup
+        Pickup pickup = other.GetComponent<Pickup>();
+        if (pickup != null)
+        {
+            nearbyPickup = pickup;
+        }
 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Pickup pickup = other.GetComponent<Pickup>();
+        if (pickup != null && pickup == nearbyPickup)
+        {
+            nearbyPickup = null;
+        }
+    }
+
+    #region MovementCalculations
+    private void UpdateCharacterVelocity(Vector3 projectedMoveDir)
+    {
+        if (direction == Vector2.zero) curSpeed = 0.0f;
+        else if (curSpeed == 0.0f) curSpeed = initSpeed;
+        else curSpeed = Mathf.MoveTowards(curSpeed, maxSpeed, moveAccel * Time.fixedDeltaTime);
+
+        velocity.x = projectedMoveDir.x * curSpeed;
+        velocity.z = projectedMoveDir.z * curSpeed;
+
+        if (!cc.isGrounded) velocity.y += gravity * Time.fixedDeltaTime;
+        else velocity.y = CheckJump();
+
+    }
+    private Vector3 ProjectedMoveDirection()
+    {
+        Vector3 cameraFwd = mainCamera.transform.forward;
+        Vector3 cameraRight = mainCamera.transform.right;
+
+        cameraFwd.y = 0;
+        cameraRight.y = 0;
+
+        cameraFwd.Normalize();
+        cameraRight.Normalize();
+
+        return cameraFwd * direction.y + cameraRight * direction.x;
+    }
+    #endregion
+
+
+    #region JumpCalculations
+    float CheckJump() => jumpPressed ? initialJumpVelocity : -cc.skinWidth;
+    void CalculateJumpVariables()
+    {
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        initialJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+    #endregion
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Debug.Log("Hit Object:" + hit.gameObject.name);
+
+        if (hit.gameObject.layer == enemyLayerMask)
+        {
+            Debug.Log("Hit an enemy!");
+        }
+
+        if (hit.gameObject.layer == weaponLayerMask && curWeapon == null)
+        {
+            WeaponBase weapon = hit.collider.GetComponent<WeaponBase>();
+            if (weapon != null)
+            {
+                curWeapon = weapon;
+                curWeapon.Equip(GetComponent<Collider>(), weaponAttachPoint);
+            }
+
+            Debug.Log($"Picked up a weapon! {weapon}");
+        }
     }
 }
